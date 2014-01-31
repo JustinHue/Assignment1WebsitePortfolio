7a1bd66a4a64d69d5bf02e3e9644a4cfa32b3f0a
JustinHue <justinhellsten@gmail.com>
2014-01-31 18:34:35 -0500

    Version 0.1 - Kick off
    - Using Foundation framework
    - Added key directories, js/css/includes etc.

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Justin Hellsten's Website</title>
+
+	<!-- If you are using CSS version, only link these 2 files, you may add app.css to use for your overrides if you like. -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/foundation.css">
+
+	<!-- If you are using the gem version, you need this only -->
+	<!-- <link rel="stylesheet" href="css/app.css"> -->
+	<script src="js/vendor/modernizr.js"></script>
+
+</head>
+
+<body>
+
+<!-- Top Nav Bar -->
+<nav id="top-nav-bar">
+	<a href="projects.php">Projects</a>
+</nav>
+
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+	require_once($_SERVER["DOCUMENT_ROOT"] . "includes/header.php");
+?>
+
+
+
+<?php
+	require_once($_SERVER["DOCUMENT_ROOT"] . "inlcudes/footer.php");
+?>
