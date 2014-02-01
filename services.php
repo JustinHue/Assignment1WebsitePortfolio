b5d5a982efe4068179c5438ac8aef93197315339
JustinHue <justinhellsten@gmail.com>
2014-01-31 21:49:12 -0500

    Version 0.5
    -> Added service information
    -> Game Development and Web Development + Design

--- services.php
+++ services.php
@@ -1,11 +1,21 @@
 <?php
-	$left_index = 1;
-        require_once($_SERVER["DOCUMENT_ROOT"] . "includes/header.php");
+        $left_index = 1;
+        include $_SERVER["DOCUMENT_ROOT"] . "includes/header.php";
 ?>
 
-
+<div class="panel">
+	<div class="row">
+		<div class="columns">
+			<h1>List of Services</h1>
+			<h3>Game Development</h3>
+			<p>I offer services to develop and market games for android and iphone apps market. I program mostly with Java.</p>
+			<h3>Web Development + Design</h3>
+			<p>I charge $20/h for all  web development and design work but can vary depending on requirements. My skills for this arena are HTML, CSS, PHP, and MySQL. 
+		</p>
+	</div>
+</div>
 
 <?php
-        require_once($_SERVER["DOCUMENT_ROOT"] . "inlcudes/footer.php");
+        include $_SERVER["DOCUMNET_ROOT"] . "includes/bottom_include.php";
 ?>
 
